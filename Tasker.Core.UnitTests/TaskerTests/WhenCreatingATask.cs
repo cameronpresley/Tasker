b59bf9fe58d56d667a35938cbe03a85e2b6c491e
Cameron Presley <cameron@thesoftwarementor.com>
2017-07-08 09:55:14 -0400

    Created Task (#1)
    * Added Tasker.Core project with a Task class.
    
    * Added TaskStatus enum, modified Task to have a Status property.
    
    * Added unit tests to Task
    
    * ReSharper cleanup.

---
+++ Tasker.Core.UnitTests/TaskerTests/WhenCreatingATask.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace Tasker.Core.UnitTests.TaskerTests
+{
+    [TestFixture]
+    public class WhenCreatingATask
+    {
+        [Test]
+        public void AndTheNameIsNullThenAnExceptionIsThrown()
+        {
+            Assert.Throws<ArgumentException>(() => Task.Create(null, "desc"));
+        }
+
+        [Test]
+        public void AndTheNameIsEmptyThenAnExceptionIsThrown()
+        {
+            Assert.Throws<ArgumentException>(() => Task.Create("", "desc"));
+        }
+
+        [Test]
+        public void AndTheDescriptionIsNullThenAnExceptionIsThrown()
+        {
+            Assert.Throws<ArgumentException>(() => Task.Create("title", null));
+        }
+
+        [Test]
+        public void AndTheDescriptionIsEmptyThenAnExceptionIsThrown()
+        {
+            Assert.Throws<ArgumentException>(() => Task.Create("title", ""));
+        }
+
+        [Test]
+        public void ThenTheTaskIsCreated()
+        {
+            var task = Task.Create("title", "desc");
+
+            Assert.AreEqual("title", task.Title);
+            Assert.AreEqual("desc", task.Description);
+            Assert.AreEqual(-1, task.Id);
+            Assert.AreEqual(TaskStatus.ToDo, task.Status);
+        }
+    }
+}
---
+++ Tasker.Core/Task.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Tasker.Core
+{
+    public class Task
+    {
+        public string Title { get; }
+        public string Description { get; }
+        public TaskStatus Status { get; }
+        public readonly int Id;
+
+        private Task(int id, string title, string description, TaskStatus status)
+        {
+            Id = id;
+            Title = title;
+            Description = description;
+            Status = status;
+        }
+
+        public static Task Create(string title, string description)
+        {
+            if (String.IsNullOrWhiteSpace(title)) throw new ArgumentException(nameof(title) + " must be specified");
+            if (String.IsNullOrWhiteSpace(description)) throw new ArgumentException(nameof(description) + " must be specified");
+            return new Task(-1, title, description, TaskStatus.ToDo);
+        }
+    }
+}
