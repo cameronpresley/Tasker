fdc9d268aa796ba2382926c59afa6cd6863a16d5
Cameron Presley <Cameron@TheSoftwareMentor.com>
2017-07-08 09:40:24 -0400

    Added Tasker.Core project with a Task class.

---
+++ Tasker.Core/Task.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Tasker.Core
+{
+    public class Task
+    {
+        public string Title { get; }
+        public string Description { get; }
+        public readonly int Id;
+
+        private Task(int id, string title, string description)
+        {
+            Id = id;
+            Title = title;
+            Description = description;
+        }
+
+        public static Task Create(string title, string description)
+        {
+            if (String.IsNullOrWhiteSpace(title)) throw new ArgumentException(nameof(title) + " must be specified");
+            if (String.IsNullOrWhiteSpace(description)) throw new ArgumentException(nameof(description) + " must be specified");
+            return new Task(-1, title, description);
+        }
+    }
+}
